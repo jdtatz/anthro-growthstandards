f7dad15f2b4d7040492d7f91dd56a1e7e6bd44ca
Julia Tatz <tatz.j@northeastern.edu>
2023-06-28 14:52:54 -0400

    Only univarate marginals & full log* impls
    don't actually need/want tanh-sinh quadrature, as only have discrete pts

--- growthstandards/xr_stats_ext.py
+++ growthstandards/xr_stats_ext.py
@@ -1,8 +1,10 @@
+from functools import cached_property, partial
 from itertools import chain
 from typing import Hashable, Optional, Sequence, Union
 
 import numpy as np
 import scipy.optimize as optimize
+import scipy.special as sc
 import scipy.stats as stats
 import xarray as xr
 import xarray_einstats.stats as xr_stats
@@ -46,6 +48,50 @@ def rv_coords(rv: xr_stats.XrRV) -> xr.Dataset:
         compat="broadcast_equals",
         combine_attrs="drop_conflicts",
     )
+
+
+def log_trapz(log_y, x=None, dx=1.0, axis=-1):
+    """Log Integrate along the given axis using the composite trapezoidal rule over the log of the integrand."""
+    log_y = np.asanyarray(log_y)
+    if x is None:
+        d = dx
+    else:
+        x = np.asanyarray(x)
+        if x.ndim == 1:
+            d = np.diff(x)
+            # reshape to correct shape
+            shape = [1] * log_y.ndim
+            shape[axis] = d.shape[0]
+            d = d.reshape(shape)
+        else:
+            d = np.diff(x, axis=axis)
+    nd = log_y.ndim
+    slice1 = [slice(None)] * nd
+    slice2 = [slice(None)] * nd
+    slice1[axis] = slice(1, None)
+    slice2[axis] = slice(None, -1)
+    return sc.logsumexp(
+        np.logaddexp(log_y[tuple(slice1)], log_y[tuple(slice2)]), b=d / 2.0, axis=axis
+    )
+
+
+def xr_log_integrate(self: xr.DataArray, *coords: Hashable):
+    from xarray.core.variable import Variable
+
+    result = self
+    for coord in coords:
+        if coord not in self.coords and coord not in self.dims:
+            raise ValueError(f"Coordinate {coord} does not exist.")
+        coord_var: Variable = self[coord].variable
+        if coord_var.ndim != 1:
+            raise ValueError(
+                "Coordinate {} must be 1 dimensional but is {}"
+                " dimensional".format(coord, coord_var.ndim)
+            )
+        dim = coord_var.dims[0]
+        # use partial instead of reduce(**kwargs) to prevent future collisions
+        result = result.reduce(partial(log_trapz, x=coord_var.data), dim=dim)
+    return result
 
 
 def _vectorized_root_scalar(f, x0, bracket, fprime):
@@ -105,169 +151,82 @@ class XrCompoundRV:
         self,
         cond_rv: xr_stats.XrContinuousRV,
         marginal_rv: xr_stats.XrContinuousRV,
-        marginal: Optional[Union[xr.DataArray, xr.Dataset]] = None,
-        *,
-        marginal_dim: Optional[Union[Hashable, Sequence[Hashable]]] = None,
+        marginal_coord: Hashable,
     ):
-        if marginal is None and marginal_dim is None:
-            raise ValueError("one of `marginal` or `marginal_dim` must not be `None`")
-        if marginal_dim is None:
-            self.marginal_dim = marginal.dims
-        elif isinstance(marginal_dim, Hashable):
-            self.marginal_dim = [marginal_dim]
-        else:
-            self.marginal_dim = marginal_dim
-        if marginal is None:
-            self.marginal = rv_coords(cond_rv)[self.marginal_dim]
-        else:
-            self.marginal = marginal
+        self.coord = marginal_coord
+        self.marginal = rv_coords(cond_rv)[marginal_coord]
         self.cond_rv = cond_rv
         self.marginal_rv = marginal_rv
-        self.p_marginal: xr.DataArray = self.marginal_rv.pdf(self.marginal)
+
+    @cached_property
+    def p_marginal(self) -> xr.DataArray:
+        return self.marginal_rv.pdf(self.marginal)
+
+    @cached_property
+    def logp_marginal(self) -> xr.DataArray:
+        return self.marginal_rv.logpdf(self.marginal)
 
     def pdf(self, x: Union[float, xr.DataArray]):
         p_x: xr.DataArray = self.cond_rv.pdf(x)
-        return (p_x * self.p_marginal).integrate(self.marginal_dim)
+        return (p_x * self.p_marginal).integrate(self.coord)
 
     def cdf(self, x: Union[float, xr.DataArray]):
         cdf_x: xr.DataArray = self.cond_rv.cdf(x)
-        return (cdf_x * self.p_marginal).integrate(self.marginal_dim)
+        return (cdf_x * self.p_marginal).integrate(self.coord)
 
     def sf(self, x: Union[float, xr.DataArray]):
         sf_x: xr.DataArray = self.cond_rv.sf(x)
-        return (sf_x * self.p_marginal).integrate(self.marginal_dim)
+        return (sf_x * self.p_marginal).integrate(self.coord)
 
     def ppf(self, q: Union[float, xr.DataArray]):
         ppf_q = self.cond_rv.ppf(q)
-        x_0: xr.DataArray = ppf_q.weighted(self.p_marginal).mean(self.marginal_dim)
+        x_0: xr.DataArray = ppf_q.weighted(self.p_marginal).mean(self.coord)
         shape_ds = rv_to_ds(self.cond_rv)
         shape_params = list(shape_ds.values())
         return xr.apply_ufunc(
             _compound_ppf_root,
             q,
             x_0,
-            ppf_q.min(self.marginal_dim),
-            ppf_q.max(self.marginal_dim),
+            ppf_q.min(self.coord),
+            ppf_q.max(self.coord),
             self.marginal,
             self.p_marginal,
             *shape_params,
-            input_core_dims=[[], [], [], [], self.marginal_dim, self.marginal_dim]
-            + [self.marginal_dim] * len(shape_params),
+            input_core_dims=[[], [], [], [], [self.coord], [self.coord]]
+            + [[self.coord]] * len(shape_params),
             kwargs=dict(dist=self.cond_rv.dist, shape_keys=shape_ds.keys()),
         )
 
     def isf(self, q: Union[float, xr.DataArray]):
         isf_q = self.cond_rv.isf(q)
-        x_0: xr.DataArray = isf_q.weighted(self.p_marginal).mean(self.marginal_dim)
+        x_0: xr.DataArray = isf_q.weighted(self.p_marginal).mean(self.coord)
         shape_ds = rv_to_ds(self.cond_rv)
         shape_params = list(shape_ds.values())
         return xr.apply_ufunc(
             _compound_isf_root,
             q,
             x_0,
-            isf_q.min(self.marginal_dim),
-            isf_q.max(self.marginal_dim),
+            isf_q.min(self.coord),
+            isf_q.max(self.coord),
             self.marginal,
             self.p_marginal,
             *shape_params,
-            input_core_dims=[[], [], [], [], self.marginal_dim, self.marginal_dim]
-            + [self.marginal_dim] * len(shape_params),
+            input_core_dims=[[], [], [], [], [self.coord], [self.coord]]
+            + [[self.coord]] * len(shape_params),
             kwargs=dict(dist=self.cond_rv.dist, shape_keys=shape_ds.keys()),
         )
 
     def median(self):
         return self.ppf(0.5)
 
-    # TODO: fully impl the log* functions once tanh-sinh quadrature is added to scipy,
-    #  which allows evaluating the log of the integral based on the log of the integrand
-    #  https://github.com/scipy/scipy/pull/18650
-
     def logpdf(self, x: Union[float, xr.DataArray]):
-        return np.log(self.pdf(x))
+        logp_x: xr.DataArray = self.cond_rv.logpdf(x)
+        return xr_log_integrate(logp_x + self.logp_marginal, self.coord)
 
     def logcdf(self, x: Union[float, xr.DataArray]):
-        return np.log(self.cdf(x))
+        logcdf_x: xr.DataArray = self.cond_rv.logcdf(x)
+        return xr_log_integrate(logcdf_x + self.logp_marginal, self.coord)
 
     def logsf(self, x: Union[float, xr.DataArray]):
-        return np.log(self.sf(x))
-
-
-def compound_pdf(
-    x: xr.DataArray,
-    cond_rv: xr_stats.XrContinuousRV,
-    marginal_rv: xr_stats.XrContinuousRV,
-    marginal: xr.DataArray,
-    *,
-    marginal_dim=None,
-):
-    if marginal_dim is None:
-        marginal_dim = marginal.dims
-    elif isinstance(marginal_dim, str):
-        marginal_dim = [marginal_dim]
-    p_x: xr.DataArray = cond_rv.pdf(x)
-    p_marginal = marginal_rv.pdf(marginal)
-    return (p_x * p_marginal).integrate(marginal_dim)
-
-
-def compound_cdf(
-    x: xr.DataArray,
-    cond_rv: xr_stats.XrContinuousRV,
-    marginal_rv: xr_stats.XrContinuousRV,
-    marginal: xr.DataArray,
-    *,
-    marginal_dim=None,
-):
-    if marginal_dim is None:
-        marginal_dim = marginal.dims
-    elif isinstance(marginal_dim, str):
-        marginal_dim = [marginal_dim]
-    cdf_x: xr.DataArray = cond_rv.cdf(x)
-    p_marginal = marginal_rv.pdf(marginal)
-    return (cdf_x * p_marginal).integrate(marginal_dim)
-
-
-def compound_ppf(
-    q: xr.DataArray,
-    cond_rv: xr_stats.XrContinuousRV,
-    marginal_rv: xr_stats.XrContinuousRV,
-    marginal: xr.DataArray,
-    *,
-    marginal_dim=None,
-):
-    def inner_compound_ppf(
-        q, x_0, x_min, x_max, m, p_m, *shape_params, dist, shape_keys
-    ):
-        kwds = dict(zip(shape_keys, shape_params))
-
-        def fun(x):
-            return np.trapz(p_m * dist.cdf(x[..., None], **kwds), m) - q
-
-        def dfun(x):
-            return np.trapz(p_m * dist.pdf(x[..., None], **kwds), m)
-
-        # return optimize.root_scalar(fun, x0=x_0, bracket=(x_min, x_max), fprime=dfun)
-        # return optimize._chandrupatla(fun, x_min, x_max)
-        return np.clip(optimize.newton(fun, x_0, fprime=dfun), x_min, x_max)
-
-    if marginal_dim is None:
-        marginal_dim = marginal.dims
-    elif isinstance(marginal_dim, str):
-        marginal_dim = [marginal_dim]
-    ppf_q = cond_rv.ppf(q)
-    p_marginal = marginal_rv.pdf(marginal)
-    x_0: xr.DataArray = ppf_q.weighted(p_marginal).mean(marginal_dim)
-    shape_ds = rv_to_ds(cond_rv)
-    shape_params = list(shape_ds.values())
-    return xr.apply_ufunc(
-        inner_compound_ppf,
-        q,
-        x_0,
-        ppf_q.min(marginal_dim),
-        ppf_q.max(marginal_dim),
-        marginal,
-        p_marginal,
-        *shape_params,
-        input_core_dims=[[], [], [], [], marginal_dim, marginal_dim]
-        + [marginal_dim] * len(shape_params),
-        kwargs=dict(dist=cond_rv.dist, shape_keys=shape_ds.keys()),
-    )
+        logsf_x: xr.DataArray = self.cond_rv.logsf(x)
+        return xr_log_integrate(logsf_x + self.logp_marginal, self.coord)
