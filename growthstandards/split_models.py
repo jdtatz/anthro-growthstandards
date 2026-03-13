ce90b571c7a3075cba0d874974ff1ea32fb3c56b
Julia Tatz <julia.tatz@childrens.harvard.edu>
2026-03-13 17:31:01 -0400

    split bmi models

--- growthstandards/split_models.py
+++ growthstandards/split_models.py
@@ -1,11 +1,25 @@
-from .gamlss_ext import GAMLSSModelByCondition, SimpleBCCGModel
+from .gamlss_ext import BCCGModel, GAMLSSModelByCondition, SimpleBCCGModel
 from .gamlss_params import LookupTable
-from .who_models import lhfa_female, lhfa_male
+from .who_models import bfa_female, bfa_male, lhfa_female, lhfa_male
 
-__all__ = ["hfa", "hfa_female", "hfa_male", "lfa", "lfa_female", "lfa_male"]
+__all__ = [
+    "bhfa",
+    "bhfa_female",
+    "bhfa_male",
+    "blfa",
+    "blfa_female",
+    "blfa_male",
+    "hfa",
+    "hfa_female",
+    "hfa_male",
+    "lfa",
+    "lfa_female",
+    "lfa_male",
+]
 
 ## split lhfa into lfa and hfa
 
+assert isinstance(lhfa_female, SimpleBCCGModel)
 assert isinstance(lhfa_female.loc, LookupTable) and lhfa_female.loc.xp[731] == 731
 assert isinstance(lhfa_female.scale, LookupTable) and lhfa_female.scale.xp[731] == 731
 lfa_female = SimpleBCCGModel(
@@ -20,6 +34,7 @@ hfa_female = SimpleBCCGModel(
     attrs={**lhfa_female.attrs, "name": "height", "long_name": "Standing Height"},
     x_attrs=lhfa_female.x_attrs,
 )
+assert isinstance(lhfa_male, SimpleBCCGModel)
 assert isinstance(lhfa_male.loc, LookupTable) and lhfa_male.loc.xp[731] == 731
 assert isinstance(lhfa_male.scale, LookupTable) and lhfa_male.scale.xp[731] == 731
 lfa_male = SimpleBCCGModel(
@@ -37,3 +52,47 @@ hfa_male = SimpleBCCGModel(
 
 lfa = GAMLSSModelByCondition(lfa_female, lfa_male, cond_attrs={"name": "is_female", "long_name": "sex = Female"})
 hfa = GAMLSSModelByCondition(hfa_female, hfa_male, cond_attrs={"name": "is_female", "long_name": "sex = Female"})
+
+## split bfa into blfa and bhfa
+
+assert isinstance(bfa_female, BCCGModel)
+assert isinstance(bfa_female.mu, LookupTable) and bfa_female.mu.xp[731] == 731
+assert isinstance(bfa_female.sigma, LookupTable) and bfa_female.sigma.xp[731] == 731
+assert isinstance(bfa_female.nu, LookupTable) and bfa_female.nu.xp[731] == 731
+# NOTE: `bhfa_female.nu` was fit as a constant
+assert (bfa_female.nu.fp[731] == bfa_female.nu.fp[731:]).all()
+blfa_female = BCCGModel(
+    mu=bfa_female.mu[:731],
+    sigma=bfa_female.sigma[:731],
+    nu=bfa_female.nu[:731],
+    attrs={**bfa_female.attrs, "long_name": "Body Mass Index (Recumbent Length)"},
+    x_attrs=bfa_female.x_attrs,
+)
+bhfa_female = BCCGModel(
+    mu=bfa_female.mu[731:],
+    sigma=bfa_female.sigma[731:],
+    nu=bfa_female.nu.fp[731],
+    attrs={**bfa_female.attrs, "long_name": "Body Mass Index (Standing Height)"},
+    x_attrs=bfa_female.x_attrs,
+)
+assert isinstance(bfa_male, BCCGModel)
+assert isinstance(bfa_male.mu, LookupTable) and bfa_male.mu.xp[731] == 731
+assert isinstance(bfa_male.sigma, LookupTable) and bfa_male.sigma.xp[731] == 731
+assert isinstance(bfa_male.nu, LookupTable) and bfa_male.nu.xp[731] == 731
+blfa_male = BCCGModel(
+    mu=bfa_male.mu[:731],
+    sigma=bfa_male.sigma[:731],
+    nu=bfa_male.nu[:731],
+    attrs={**bfa_male.attrs, "long_name": "Body Mass Index (Recumbent Length)"},
+    x_attrs=bfa_male.x_attrs,
+)
+bhfa_male = BCCGModel(
+    mu=bfa_male.mu[731:],
+    sigma=bfa_male.sigma[731:],
+    nu=bfa_male.nu[731:],
+    attrs={**bfa_male.attrs, "long_name": "Body Mass Index (Standing Height)"},
+    x_attrs=bfa_male.x_attrs,
+)
+
+blfa = GAMLSSModelByCondition(blfa_female, blfa_male, cond_attrs={"name": "is_female", "long_name": "sex = Female"})
+bhfa = GAMLSSModelByCondition(bhfa_female, bhfa_male, cond_attrs={"name": "is_female", "long_name": "sex = Female"})
